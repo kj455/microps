361fedaa4c5ce0f3f149ef86ce249eb93864facc
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 07:38:50 +0900

    Skeleton of step8

--- icmp.c
+++ icmp.c
@@ -5,10 +5,47 @@
 #include "ip.h"
 #include "icmp.h"
 
-static void icmp_input(const struct ip_hdr *iphdr, const uint8_t *data,
-                       size_t len, struct ip_iface *iface) {
-  char addr1[IP_ADDR_STR_LEN];
-  char addr2[IP_ADDR_STR_LEN];
+#define icmp_type com.type
+#define icmp_code com.code
+#define icmp_sum  com.sum
+
+struct icmp_common {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+};
+
+struct icmp_hdr {
+    struct icmp_common com;
+    uint32_t dep; /* message dependent field*/
+};
+
+struct icmp_echo {
+    struct icmp_common com;
+    uint16_t id;
+    uint16_t seq;
+};
+
+struct icmp_dest_unreach {
+    struct icmp_common com;
+    uint32_t unused; /* zero */
+};
+
+static char *
+icmp_type_ntoa(uint8_t type)
+{
+}
+
+static void
+icmp_print(const uint8_t *data, size_t len)
+{
+}
+
+static void
+icmp_input(const struct ip_hdr *iphdr, const uint8_t *data, size_t len, struct ip_iface *iface)
+{
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
 
   debugf("%s => %s, len=%zu", ip_addr_ntop(iphdr->src, addr1, sizeof(addr1)),
          ip_addr_ntop(iphdr->dst, addr2, sizeof(addr2)), len);
