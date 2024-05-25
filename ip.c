ab99c182f660aeefbbbfd34d519dc03dc1b52ce5
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 07:27:07 +0900

    Skeleton of step7

--- ip.c
+++ ip.c
@@ -17,6 +17,12 @@
 
 #define IP_HDR_OFFSET_MASK 0x1fff
 
+struct ip_protocol {
+    struct ip_protocol *next;
+    uint8_t protocol;
+    ip_protocol_handler_t handler;
+};
+
 const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
@@ -25,6 +31,7 @@ const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
  *       you need to protect these lists with a mutex.
  */
 static struct ip_iface *ifaces;
+static struct ip_protocol *protocols;
 
 int
 ip_addr_pton(const char *p, ip_addr_t *n)
@@ -148,13 +155,21 @@ static void ip_print(const uint8_t *data, size_t len) {
           ip_addr_ntop(hdr->src, addr, sizeof(addr)));
   fprintf(stderr, "        dst: %s\n",
           ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
-
 #ifdef HEXDUMP
   hexdump(stderr, data, len);
 #endif
 
   funlockfile(stderr);
 }
+
+/*
+ * NOTE: must not be call after net_run()
+ */
+int
+ip_protocol_register(uint8_t protocol, ip_protocol_handler_t handler)
+{
+}
+
 
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
