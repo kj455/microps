aa4d481d4e49bd5395618318bbcfd6fabf11c5fc
kj455 <kaji.ibuki45@gmail.com>
2025-12-07 14:14:37 +0900

    feat: ip packet verification

--- ip.c
+++ ip.c
@@ -51,16 +51,85 @@ ip_addr_ntop(ip_addr_t n, char *p, size_t size)
     return p;
 }
 
-static void
-ip_print(const uint8_t *data, size_t len)
-{
+static void ip_print(const uint8_t *data, size_t len) {
+  struct ip_hdr *hdr;
+  uint8_t v, hl, hlen;
+  uint16_t total, offset;
+  char addr[IP_ADDR_STR_LEN];
+
+  flockfile(stderr);
+  hdr = (struct ip_hdr *)data;
+  v = hdr->vhl >> 4;
+  hl = hdr->vhl & 0x0f;
+  hlen = hl * 4;
+  fprintf(stderr, "        vhl: 0x%02x [v: %u, hl: %u, hlen: %u]\n", hdr->vhl,
+          v, hl, hlen);
+  fprintf(stderr, "        tos: 0x%02x\n", hdr->tos);
+  total = ntoh16(hdr->total);
+  fprintf(stderr, "        total: %u (payload: %u)\n", total, total - hlen);
+  fprintf(stderr, "        id: %u\n", ntoh16(hdr->id));
+  offset = ntoh16(hdr->offset);
+  fprintf(stderr, "        offset: 0x%04x [flags=0x%02x, offset=0x%02x]\n",
+          offset, offset >> 13, offset & IP_HDR_OFFSET_MASK);
+  fprintf(stderr, "        ttl: %u\n", hdr->ttl);
+  fprintf(stderr, "        protocol: %u\n", hdr->protocol);
+  fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+  fprintf(stderr, "        src: %s\n",
+          ip_addr_ntop(hdr->src, addr, sizeof(addr)));
+  fprintf(stderr, "        dst: %s\n",
+          ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+
+#ifdef HEXDUMP
+  hexdump(stderr, data, len);
+#endif
+
+  funlockfile(stderr);
 }
 
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, len=%zu", dev->name, len);
-    debugdump(data, len);
+  struct ip_hdr *hdr;
+  uint8_t v;
+  uint16_t hlen, total, offset;
+
+  debugf("dev=%s, len=%zu", dev->name, len);
+  // check header length
+  if (len < IP_HDR_SIZE_MIN) {
+    errorf("too short");
+    return;
+  }
+  hdr = (struct ip_hdr *)data;
+  v = hdr->vhl >> 4;
+  // check version
+  if (v != IP_VERSION_IPV4) {
+    errorf("invalid version");
+    return;
+  }
+  hlen = (hdr->vhl & 0x0f) << 2;
+  // check header length
+  if (len < hlen) {
+    errorf("header length error: len=%zu, hlen=%u", len, hlen);
+    return;
+  }
+  // check checksum
+  if (cksum16((uint16_t *)hdr, hlen, 0) != 0) {
+    errorf("checksum error");
+    return;
+  }
+  total = ntoh16(hdr->total);
+  // check total length
+  if (len < total) {
+    errorf("total length error: len=%zu, total=%u", len, total);
+    return;
+  }
+  offset = ntoh16(hdr->offset);
+  // check fragment
+  if (offset & IP_HDR_FLAG_MF || offset & IP_HDR_OFFSET_MASK) {
+    errorf("fragmented packet not supported");
+    return;
+  }
+  ip_print(data, total);
 }
 
 int ip_init(void) {
--- ip.h
+++ ip.h
@@ -18,16 +18,16 @@
 typedef uint32_t ip_addr_t;
 
 struct ip_hdr {
-    uint8_t vhl;
-    uint8_t tos;
-    uint16_t total;
-    uint16_t id;
-    uint16_t offset;
-    uint8_t ttl;
-    uint8_t protocol;
-    uint16_t sum;
-    ip_addr_t src;
-    ip_addr_t dst;
+  uint8_t vhl; // version(4) + header length(4)
+  uint8_t tos;
+  uint16_t total;
+  uint16_t id;
+  uint16_t offset; // flag(3) + fragment offset(13)
+  uint8_t ttl;
+  uint8_t protocol;
+  uint16_t sum;
+  ip_addr_t src;
+  ip_addr_t dst;
 };
 
 extern const ip_addr_t IP_ADDR_ANY;
