3238dbf69f6c6ca18d79f1cf5ef489f67daf5eb6
kj455 <kaji.ibuki45@gmail.com>
2025-12-14 09:49:05 +0000

    feat: udp init

--- net.c
+++ net.c
@@ -9,6 +9,7 @@
 #include "ip.h"
 #include "net.h"
 #include "platform.h"
+#include "udp.h"
 #include "util.h"
 
 struct net_protocol {
@@ -269,6 +270,10 @@ int net_init(void) {
   }
   if (icmp_init() == -1) {
     errorf("icmp_init() failure");
+    return -1;
+  }
+  if (udp_init() == -1) {
+    errorf("udp_init() failure");
     return -1;
   }
   infof("success");
