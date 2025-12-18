b3e5b2b3ff46a6f5c11f0a0828640c855ffaacf2
kj455 <kaji.ibuki45@gmail.com>
2025-12-18 20:52:14 +0900

    feat: tcp init

--- net.c
+++ net.c
@@ -9,6 +9,7 @@
 #include "ip.h"
 #include "net.h"
 #include "platform.h"
+#include "tcp.h"
 #include "udp.h"
 #include "util.h"
 
@@ -274,6 +275,10 @@ int net_init(void) {
   }
   if (udp_init() == -1) {
     errorf("udp_init() failure");
+    return -1;
+  }
+  if (tcp_init() == -1) {
+    errorf("tcp_init() failure");
     return -1;
   }
   infof("success");
