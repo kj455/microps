683221f6cb3ec1c2df5dda933f1636f6d1a44a37
kj455 <kaji.ibuki45@gmail.com>
2025-12-20 01:11:37 +0000

    feat: tcp pcb

--- tcp.c
+++ tcp.c
@@ -9,15 +9,16 @@
 
 #include "ip.h"
 #include "net.h"
+#include "sched.h"
 #include "tcp.h"
 #include "util.h"
 
-#define TCP_FLG_FIN 0x01
-#define TCP_FLG_SYN 0x02
-#define TCP_FLG_RST 0x04
-#define TCP_FLG_PSH 0x08
-#define TCP_FLG_ACK 0x10
-#define TCP_FLG_URG 0x20
+#define TCP_FLG_FIN 0x01 // finish
+#define TCP_FLG_SYN 0x02 // synchronize
+#define TCP_FLG_RST 0x04 // reset
+#define TCP_FLG_PSH 0x08 // push
+#define TCP_FLG_ACK 0x10 // acknowledgment
+#define TCP_FLG_URG 0x20 // urgent
 
 #define TCP_FLG_IS(x, y) ((x & 0x3f) == (y))
 #define TCP_FLG_ISSET(x, y) ((x & 0x3f) & (y) ? 1 : 0)
@@ -64,6 +65,7 @@ struct tcp_hdr {
   uint16_t up;
 };
 
+// 送信シーケンス変数
 struct snd_vars {
   uint32_t nxt;
   uint32_t una;
@@ -73,6 +75,7 @@ struct snd_vars {
   uint32_t wl2;
 };
 
+// 受信シーケンス変数
 struct rcv_vars {
   uint32_t nxt;
   uint16_t wnd;
@@ -93,11 +96,11 @@ struct tcp_pcb {
 };
 
 struct seg_info {
-  uint32_t seq;
-  uint32_t ack;
-  uint16_t len;
-  uint16_t wnd;
-  uint16_t up;
+  uint32_t seq; // sequence number
+  uint32_t ack; // acknowledgment number
+  uint16_t len; // consumed length of sequence space
+  uint16_t wnd; // window size
+  uint16_t up;  // urgent pointer
 };
 
 static lock_t lock = LOCK_INITIALIZER; /* for PCBs*/
@@ -216,24 +219,133 @@ static void tcp_print(const uint8_t *data, size_t len) {
  * NOTE: TCP PCB functions must be called after locked
  */
 
-static int tcp_pcb_desc(struct tcp_pcb *pcb) {}
+static int tcp_pcb_desc(struct tcp_pcb *pcb) { return indexof(pcbs, pcb); }
 
-static struct tcp_pcb *tcp_pcb_get(int desc) {}
+static struct tcp_pcb *tcp_pcb_get(int desc) {
+  struct tcp_pcb *pcb;
+  if (desc < 0 || (int)countof(pcbs) <= desc) {
+    // out of range
+    return NULL;
+  }
+  pcb = &pcbs[desc];
+  if (pcb->state == TCP_STATE_NONE) {
+    return NULL;
+  }
+  return pcb;
+}
 
-static struct tcp_pcb *tcp_pcb_alloc(void) {}
+static struct tcp_pcb *tcp_pcb_alloc(void) {
+  struct tcp_pcb *pcb;
+  for (pcb = pcbs; pcb < tailof(pcbs); pcb++) {
+    if (pcb->state == TCP_STATE_NONE) {
+      pcb->state = TCP_STATE_CLOSED;
+      sched_task_init(&pcb->task);
+      return pcb;
+    }
+  }
+  return NULL;
+}
 
-static void tcp_pcb_release(struct tcp_pcb *pcb) {}
+static void tcp_pcb_release(struct tcp_pcb *pcb) {
+  if (sched_task_destroy(&pcb->task) != 0) {
+    debugf("pending, desc=%d", tcp_pcb_desc(pcb));
+    sched_task_wakeup(&pcb->task);
+    return;
+  }
+  memset(pcb, 0, sizeof(*pcb));
+  debugf("success, desc=%d", tcp_pcb_desc(pcb));
+}
 
-static struct tcp_pcb *tcp_pcb_select(ip_endp_t key1, ip_endp_t key2) {}
+static struct tcp_pcb *tcp_pcb_select(ip_endp_t key1, ip_endp_t key2) {
+  struct tcp_pcb *pcb, *candidate = NULL;
+  for (pcb = pcbs; pcb < tailof(pcbs); pcb++) {
+    if (pcb->local.port != key1.port) {
+      continue;
+    }
+    if (pcb->local.addr == key1.addr || pcb->local.addr == IP_ADDR_ANY ||
+        key1.addr != IP_ADDR_ANY) {
+      if ((pcb->remote.port == key2.port && pcb->remote.addr == key2.addr) ||
+          (pcb->remote.port == 0 && pcb->remote.addr == IP_ADDR_ANY) ||
+          (key2.addr == IP_ADDR_ANY && key2.port == 0)) {
+        // exact match
+        if (pcb->state != TCP_STATE_LISTEN) {
+          return pcb;
+        }
+        candidate = pcb;
+      }
+    }
+  }
+  return candidate;
+}
 
 static ssize_t tcp_output_segment(uint32_t seq, uint32_t ack, uint8_t flg,
                                   uint16_t wnd, const uint8_t *data, size_t len,
-                                  ip_endp_t local, ip_endp_t remote) {}
+                                  ip_endp_t local, ip_endp_t remote) {
+  uint8_t buf[IP_PAYLOAD_SIZE_MAX] = {0};
+  struct tcp_hdr *hdr;
+  uint8_t hlen;
+  struct pseudo_hdr pseudo;
+  uint16_t psum;
+  uint16_t total;
+  char ep1[IP_ENDP_STR_LEN];
+  char ep2[IP_ENDP_STR_LEN];
+
+  hdr = (struct tcp_hdr *)buf;
+  hdr->src = local.port;
+  hdr->dst = remote.port;
+  hdr->seq = hton32(seq);
+  hdr->ack = hton32(ack);
+  hlen = sizeof(*hdr);
+  hdr->off = (hlen >> 2) << 4;
+  hdr->flg = flg;
+  hdr->wnd = hton16(wnd);
+  hdr->sum = 0;
+  hdr->up = 0;
+  memcpy((uint8_t *)hdr + hlen, data, len);
+  pseudo.src = local.addr;
+  pseudo.dst = remote.addr;
+  pseudo.zero = 0;
+  pseudo.protocol = IP_PROTOCOL_TCP;
+  total = hlen + len;
+  pseudo.len = hton16((uint16_t)total);
+  psum = ~cksum16((uint16_t *)&pseudo, sizeof(pseudo), 0);
+  hdr->sum = cksum16((uint16_t *)hdr, total, psum);
+  debugf("%s => %s, len=%zu", ip_endp_ntop(local, ep1, sizeof(ep1)),
+         ip_endp_ntop(remote, ep2, sizeof(ep2)), total);
+  tcp_print(buf, total);
+  if (ip_output(IP_PROTOCOL_TCP, buf, total, local.addr, remote.addr) == -1) {
+    return -1;
+  }
+  return len;
+}
 
 /* rfc793 - section 3.9 [Event Processing > SEGMENT ARRIVES] */
 static void tcp_segment_arrives(struct seg_info *seg, uint8_t flags,
                                 const uint8_t *data, size_t len,
-                                ip_endp_t local, ip_endp_t remote) {}
+                                ip_endp_t local, ip_endp_t remote) {
+  struct tcp_pcb *pcb;
+  pcb = tcp_pcb_select(local, remote);
+  /**
+   * ex.
+   * - there is no connection for this segment
+   * - segment for closed connection has arrived late
+   */
+  if (!pcb || pcb->state == TCP_STATE_CLOSED) {
+    debugf("PCB is %s", pcb ? "CLOSED" : "not found");
+    if (TCP_FLG_ISSET(flags, TCP_FLG_RST)) {
+      // the other side has already closed the connection, so just finish
+      return;
+    }
+    if (!TCP_FLG_ISSET(flags, TCP_FLG_ACK)) {
+      tcp_output_segment(0, seg->seq + seg->len, TCP_FLG_RST | TCP_FLG_ACK, 0,
+                         NULL, 0, local, remote);
+    } else {
+      tcp_output_segment(seg->ack, 0, TCP_FLG_RST, 0, NULL, 0, local, remote);
+    }
+    return;
+  }
+  // TODO: implement
+}
 
 static void tcp_input(const struct ip_hdr *iphdr, const uint8_t *data,
                       size_t len, struct ip_iface *iface) {
@@ -243,6 +355,8 @@ static void tcp_input(const struct ip_hdr *iphdr, const uint8_t *data,
   ip_endp_t src, dst;
   char ep1[IP_ENDP_STR_LEN];
   char ep2[IP_ENDP_STR_LEN];
+  uint8_t hlen;
+  struct seg_info seg;
 
   if (len < sizeof(*hdr)) {
     errorf("too short");
@@ -274,6 +388,20 @@ static void tcp_input(const struct ip_hdr *iphdr, const uint8_t *data,
   debugf("%s => %s, len=%zu, dev=%s", ep1, ep2, len,
          NET_IFACE(iface)->dev->name);
   tcp_print(data, len);
+  hlen = (hdr->off >> 4) << 2;
+  seg.seq = ntoh32(hdr->seq);
+  seg.ack = ntoh32(hdr->ack);
+  seg.len = len - hlen;
+  if (TCP_FLG_ISSET(hdr->flg, TCP_FLG_SYN) ||
+      TCP_FLG_ISSET(hdr->flg, TCP_FLG_FIN)) {
+    seg.len += 1; // SYN/FIN consumes one sequence number
+  }
+  seg.wnd = ntoh16(hdr->wnd);
+  seg.up = ntoh16(hdr->up);
+  lock_acquire(&lock);
+  tcp_segment_arrives(&seg, hdr->flg, (uint8_t *)hdr + hlen, len - hlen, dst,
+                      src);
+  lock_release(&lock);
   return;
 }
 
